b32d99abe17bbedb3ea3a84cbba490adf0380514
meynard <menandro.p.manuba@gmail.com>
2016-05-05 19:38:06 -0600

    Update mysqli.php

--- mysqli.php
+++ mysqli.php
@@ -1,39 +1,88 @@
 <?php
 
 class Db {
-  private static $mysqli;
-  function __construct($config) {
-    $mysqli = new mysqli("localhost", "homestead", "secret", "homestead");
+  static $mysqli;
+  static $num_rows;
+  static $query_logs = array();
+  function __construct() {
+    if (!self::$mysqli) {
+        $mysqli = new \mysqli("localhost", "mrwebstdev1206", "eawCEATT", "mrwebstdev1206");
 
-    if ($mysqli->connect_errno) {
-        echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+        if ($mysqli->connect_errno) {
+            self::$query_logs[] = "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+        }
     }
     self::$mysqli = $mysqli;
   }
 
-  private static function mysqli_prepare($sql) {
-    if (!($stmt = self::$mysqli->prepare($sql))) {
-        echo "Prepare failed: (" . self::$mysqli->errno . ") " . self::$mysqli->error;
+  private static function prepare($sql, $params = null) {
+    if (($stmt = self::$mysqli->prepare($sql))) {
+        if (is_array($params)) {
+            $a_params = array(str_repeat('s',count($params)));
+            foreach($params as $index => $param) {
+                $a_params[] = & $params[$index];
+            }
+            if (!($result = call_user_func_array(array($stmt, 'bind_param'), $a_params))) {
+                $stmt = $result;
+                self::$query_logs[] = "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+            }
+        }
+    } else {
+        self::$query_logs[] = "Prepare failed: (" . self::$mysqli->errno . ") " . self::$mysqli->error;
     }
     return $stmt;
   }
 
-  private static function mysqli_bind_param($stmt, $a_params) {
-    if (!($result = call_user_func_array([$stmt, 'bind_param'], $a_params))) {
-        echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+  public static function execute($sql, $params = null) {
+    if (($stmt = self::prepare($sql, $params))) {
+        if (!$stmt->execute()) {
+            self::$query_logs[] = "Execute failed: (" . $stmt->errno . ") " . $stmt->error . "\n>{$sql}";
+        }
     }
-    return $result;
+    return $stmt;
   }
 
-  private static function mysqli_execute($sql, $params) {
-    $stmt = self::mysqli_prepare($sql);
-    if ($stmt) {
-      if (self::mysqli_bind_param($stmt, $params)) {
-        if (!$stmt->execute()) {
-            echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+  public static function query($sql, $params = null) {
+    $data = null;
+    if (($stmt = self::execute($sql, $params))) {
+        $md = $stmt->result_metadata();
+        $fields = array();
+        while($field = $md->fetch_field()) {
+            $fields[$field->name] = &$result[$field->name];
+        }        
+        call_user_func_array(array($stmt, "bind_result"), $fields);        
+        // returns a copy of a value
+        $copy = create_function('$a', 'return $a;');
+        while ($stmt->fetch()) {
+            $data[] = array_map($copy, $fields);
         }
-      }
+        $stmt->close();
     }
+    return $data;
+  }
+
+  public static function num_rows($sql, $params = null) {
+    $num_rows = 0;
+    if (($stmt = self::execute($sql, $params))) {
+        $stmt->store_result();
+        $num_rows = $stmt->num_rows;
+        $stmt->close();
+    }
+    return $num_rows;
+  }
+  
+  public static function fetch_fields($table) {
+    $fields = array();
+    $sql = "SELECT * FROM {$table} LIMIT 0;";
+    if (($stmt = self::execute($sql))) {
+        $md = $stmt->result_metadata();
+        $fields = array();
+        while($field = $md->fetch_field()) {
+            $fields[] = $field->name;
+        }
+        $stmt->close();
+    }
+    return $fields;
   }
 
   static function buildSql($sql, $params) {
@@ -47,103 +96,124 @@ class Db {
     $params = array_map($func, $params);
     array_unshift($params, str_replace('?', "'%s'", $sql));
     $_sql_ = call_user_func_array('sprintf', $params);
-    echo "\nExecuted sql: {$_sql_}";
+    self::$query_logs[] = "Executed sql: {$_sql_}";
     return $_sql_;
   }
 
   static function insert($table, $data) {
-    $mysqli = self::$mysqli;
-
     $sql = "INSERT INTO {$table} SET ";
     $sql_set_fields = null;
-    $a_params = [''];
+    $a_params = null;
+    $fields = self::fetch_fields($table);
     foreach($data as $field_name => $field_value) {
-      $a_params[0] .= 's';
-      $a_params[] = & $data[$field_name];
+      if (!in_array($field_name, $fields)) continue;
+      $a_params[] = $data[$field_name];
       $sql_set_fields[] = "$field_name = ?";
     }
     $sql .= implode(", ", $sql_set_fields);
 
-    self::mysqli_execute($sql, $a_params);
+    self::execute($sql, $a_params);
 
     array_shift($a_params);
     $_sql_ = self::buildSql($sql, $a_params);
-
-    return $mysqli->insert_id;
+    
+    return self::$mysqli->insert_id;
   }
 
   static function update($table, $pkey, $data) {
-    $mysqli = self::$mysqli;
-
     $sql = "UPDATE {$table} SET ";
     $sql_set_fields = null;
-    $a_params = [''];
+    $a_params = null;
+    $fields = self::fetch_fields($table);
     foreach($data as $field_name => $field_value) {
-      if ($pkey==$field_name) continue;
-      $a_params[0] .= 's';
-      $a_params[] = & $data[$field_name];
+      if (!in_array($field_name, $fields)) continue;
+      $a_params[] = $data[$field_name];
       $sql_set_fields[] = "$field_name = ?";
     }
     $sql .= implode(", ", $sql_set_fields);
 
     $sql .= " WHERE {$pkey} = ?";
-    $a_params[0] .= 's';
-    $a_params[] = & $data[$pkey];
+    $a_params[] = $data[$pkey];
 
-    self::mysqli_execute($sql, $a_params);
+    self::execute($sql, $a_params);
 
     array_shift($a_params);
     $_sql_ = self::buildSql($sql, $a_params);
   }
 
   static function delete($table, $pkey, $data) {
-    $mysqli = self::$mysqli;
-
     $sql = "DELETE FROM {$table} ";
-    $a_params = [''];
+    $a_params = null;
 
     $sql .= " WHERE {$pkey} = ?";
-    $a_params[0] .= 's';
-    $a_params[] = & $data[$pkey];
+    $a_params[] = $data[$pkey];
 
-    self::mysqli_execute($sql, $a_params);
+    self::execute($sql, $a_params);
 
     array_shift($a_params);
     $_sql_ = self::buildSql($sql, $a_params);
+  }
+  
+  static function fetch($table, $data = null, $offset = 0, $limit = 5, $num_rows = 0) {
+    $sql = "SELECT * FROM {$table} ";
+    
+    $sql_where_fields = array();
+    $a_params = null;
+    
+    if (is_array($data)) {
+        foreach($data as $field_name => $field_value) {
+          $a_params[] = $data[$field_name];
+          $sql_where_fields[] = "$field_name = ?";
+        }
+        if (count($sql_where_fields)) {
+            $sql .= " WHERE " . implode(" AND ", $sql_where_fields);
+        }
+    }    
+    
+    $num_rows = self::num_rows($sql, $a_params);
+    return;
+    $sql .= " LIMIT {$offset}, {$limit}";
+    
+    $data = self::query($sql, $a_params);
+    
+    return $data;
   }
 
   static function run() {
     $mysqli = self::$mysqli;
 
     /* Non-prepared statement */
-    if (!$mysqli->query("TRUNCATE users")) {
-        echo "\nTable creation failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    if (!$mysqli->query("DROP TABLE IF EXISTS Customers") || !$mysqli->query("CREATE TABLE Customers(CustomerID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, FirstName VARCHAR(50), LastName VARCHAR(50))")) {
+        self::$query_logs[] = "Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error;
     }
-
     /* Non-prepared statement */
     if (!$mysqli->query("DROP TABLE IF EXISTS users") || !$mysqli->query("CREATE TABLE users(id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, username VARCHAR(50), password VARCHAR(50))")) {
-        echo "\nTable creation failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        self::$query_logs[] = "Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error;
     }
 
     /* Prepared statement, stage 1: prepare */
     if (!($stmt = $mysqli->prepare("INSERT INTO users(id, username, password) VALUES (?, ?, ?)"))) {
-        echo "\nPrepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        self::$query_logs[] = "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
     }
 
     $id = 1;
     $username = 'root';
     $password = 'secret';
 
-    if (!$stmt->bind_param("is", $id, $username, $password)) {
-        echo "\nBinding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+    if (!$stmt->bind_param("iss", $id, $username, $password)) {
+        self::$query_logs[] = "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
     }
 
     if (!$stmt->execute()) {
-      echo "\nExecute failed: (" . $stmt->errno . ") " . $stmt->error;
+      self::$query_logs[] = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
     }
   }
 }
 
+return new Db();
+
+
+/*
 
 $db = new Db(array());
 
@@ -167,3 +237,5 @@ Db::delete('users', 'id', [
   //'username' => "xem's",
   'password' => 'p@$$',
 ]);
+
+*/
